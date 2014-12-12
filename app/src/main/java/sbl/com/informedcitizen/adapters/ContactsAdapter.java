8de74646147c651870b6450228cd8b0cb4481d47
Alex Tam <alextam07@gmail.com>
2014-12-11 21:09:15 -0800

    mac user reverted the portrait implementation comment, then pushed comment about contact listener.  I am adding the implementation comment now.

--- app/src/main/java/sbl/com/informedcitizen/adapters/ContactsAdapter.java
+++ app/src/main/java/sbl/com/informedcitizen/adapters/ContactsAdapter.java
@@ -95,8 +95,7 @@ public class ContactsAdapter extends ArrayAdapter<Contact> {
 
         String currName = currContact.getName();
 
-        /* GET request to retrieve url, then another GET to render it into the ImageView
-         */
+        // GET request to retrieve url, then another GET to render it into the ImageView
         APIclient.getImageJson(getContext(), currName, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject imgJson) {
